938a13d2376b15750fd9d80571af89c905eec4f2
Melih Çakmak <melihcakmak1755@gmail.com>
2025-08-11 21:07:33 +0300

    refactoring

--- lib/core/SharedPreferencesService.dart
+++ lib/core/SharedPreferencesService.dart
@@ -3,19 +3,19 @@ import 'package:shared_preferences/shared_preferences.dart';
 class SharedPreferencesService {
   static Future<void> addFile(String key, String value) async {
     final prefs = await SharedPreferences.getInstance();
-    final downloadedVideos = prefs.getStringList(key) ?? [];
-    if (!downloadedVideos.contains(value)) {
-      downloadedVideos.add(value);
-      await prefs.setStringList(key, downloadedVideos);
+    final stringList = prefs.getStringList(key) ?? [];
+    if (!stringList.contains(value)) {
+      stringList.add(value);
+      await prefs.setStringList(key, stringList);
     }
   }
 
   static Future<void> removeFile(String key, String value) async {
     final prefs = await SharedPreferences.getInstance();
-    final downloadedVideos = prefs.getStringList(key) ?? [];
-    if (downloadedVideos.contains(value)) {
-      downloadedVideos.remove(value);
-      await prefs.setStringList(key, downloadedVideos);
+    final stringList = prefs.getStringList(key) ?? [];
+    if (stringList.contains(value)) {
+      stringList.remove(value);
+      await prefs.setStringList(key, stringList);
     }
   }
 

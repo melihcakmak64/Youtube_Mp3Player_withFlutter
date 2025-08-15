79e9834f9f316f411ea68ce7c423bd40acf55dd4
Melih Çakmak <melihcakmak1755@gmail.com>
2025-08-15 23:03:31 +0300

    Update downloadQualityDialog.dart

--- lib/view/widgets/downloadQualityDialog.dart
+++ lib/view/widgets/downloadQualityDialog.dart
@@ -84,7 +84,7 @@ class DownloadQualityDialog extends ConsumerWidget {
                     String title = "";
                     if (opt is VideoOnlyStreamInfo) {
                       title =
-                          '${opt.qualityLabel} + ${opt.framerate.framesPerSecond} (${opt.size.totalMegaBytes.toStringAsFixed(2)} MB)';
+                          '${opt.qualityLabel} (${opt.size.totalMegaBytes.toStringAsFixed(2)} MB)';
                     }
                     return ListTile(
                       title: Text(title),

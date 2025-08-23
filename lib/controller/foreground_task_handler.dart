a939d6aab9496887813b609a961254c01a6ec1b8
Melih Çakmak <melihcakmak1755@gmail.com>
2025-08-23 10:03:11 +0300

    refactoring

--- lib/controller/foreground_task_handler.dart
+++ lib/controller/foreground_task_handler.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_foreground_task/flutter_foreground_task.dart';
 import 'package:youtube_downloader/controller/download_queue_controller.dart';
 import 'package:youtube_downloader/model/DownloadTask.dart';
+import 'package:youtube_downloader/services/download_service.dart';
+import 'package:youtube_downloader/services/youtube_explode_service.dart';
 
 @pragma('vm:entry-point')
 void startCallback() {
@@ -10,6 +12,11 @@ void startCallback() {
 class MyTaskHandler extends TaskHandler {
   @override
   Future<void> onStart(DateTime timestamp, TaskStarter starter) async {
+    DownloadQueueManager.instance.init(
+      downloadService: DownloadService(
+        youtubeExplodeService: YoutubeExplodeService(),
+      ),
+    );
     FlutterForegroundTask.updateService(
       notificationTitle: 'Foreground Task',
       notificationText: 'service started',
